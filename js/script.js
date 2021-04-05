4d262c4da3e8443f91b50d5c28788efb3399453a
Alexandr <81333712+AleksanVolkov@users.noreply.github.com>
2021-04-05 14:21:08 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*alert('hello');*/
+
+/*const result = confirm ('ты еще здесь?');
+console.log(result);*/
+
+
+/*_______PROMPT___________*/
+/*const answers = [];
+
+answers [0] = prompt('как ваше имя', '');
+answers [1] = prompt('как ваша фамилия', '');
+answers [2] = prompt('сколько вам лет', '');
+
+document.write (answers);*/
+
+
+/*____________БЭКТИКИ ````__________*/
+/*const category = 'toys';
+console.log(`https://someurl.com/${category}/pony/52`);*/
+
+
+
+console.log(25*2 == 50);
